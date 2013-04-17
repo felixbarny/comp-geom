6f724c337dc27088b5e46fa62682bedc2fd2d300
Felix <felix.barnsteiner@gmail.com>
2013-04-17 19:49:48 +0200

    refactored algorithm, structured tests, added tests

--- src/intersections/Intersections.groovy
+++ src/intersections/Intersections.groovy
@@ -35,16 +35,15 @@ GParsPool.withPool {
 
     @CompileStatic boolean intersects(Stretch r) {
         def ccw1 = ccw(  p,   q, r.p) * ccw(  p,   q, r.q)
-        def ccw2 = ccw(r.p, r.q, p) * ccw(r.p, r.q, q)
+        def ccw2 = ccw(r.p, r.q,   p) * ccw(r.p, r.q,   q)
 
-        if (ccw1 <= 0 && ccw2 <= 0) {
-            if (ccw1 == 0 && ccw2 == 0) {
-                def parallel = getLeftParallelStetch()
-                if ((ccw(p, r.p, parallel.p) >= 0 && ccw(r.p, q, parallel.q) >= 0) || (ccw(p, r.q, parallel.p) >= 0 && ccw(r.q, q, parallel.q) >= 0)) {
-                    if (isPoint() && r.isPoint()) this == r
-                    else true
-                } else false
-            } else true
+        ccw1 == 0 && ccw2 == 0  ? isOverlpping(r) : ccw1 <= 0 && ccw2 <= 0
+    }
+
+    @CompileStatic boolean isOverlpping(Stretch r) {
+        def parallel = getLeftParallelStretch()
+        if ((ccw(p, r.p, parallel.p) >= 0 && ccw(r.p, q, parallel.q) >= 0) || (ccw(p, r.q, parallel.p) >= 0 && ccw(r.q, q, parallel.q) >= 0)) {
+            !(isPoint() && r.isPoint()) ?: this == r
         } else false
     }
 
@@ -52,24 +51,20 @@ GParsPool.withPool {
         (p.x * q.y - p.y * q.x) + (q.x * r.y - q.y * r.x) + (p.y * r.x - p.x * r.y)
     }
 
-    @CompileStatic Stretch getLeftParallelStetch() {
+    @CompileStatic Stretch getLeftParallelStretch() {
         def deltaX = q.x - p.x
         def deltaY = p.y - q.y
-        valueOf([p.x+deltaY, p.y + deltaX, q.x + deltaY, q.y + deltaX])
+        valueOf([p.x + deltaY, p.y + deltaX, q.x + deltaY, q.y + deltaX])
     }
 
-    @CompileStatic boolean isPoint() {
-            q == p
-        }
+    @CompileStatic boolean isPoint() { q == p }
 
     String toString() {
-        [p.x,p.y,q.x,q.y].toString()
+        [p.x, p.y, q.x, q.y].toString()
     }
 
 }
 
 @Immutable @CompileStatic class Coordinate {
-
-
     double x, y
 }
--- src/intersections/IntersectionsTest.groovy
+++ src/intersections/IntersectionsTest.groovy
@@ -4,52 +4,70 @@ package intersections
 import spock.lang.*
 
 class IntersectionsTest extends Specification {
-    def "stretch intersection test"() {
+    def "simple stretches are calculated correctly"() {
+        expect:
+        Stretch.valueOf(stretch1).intersects(Stretch.valueOf(stretch2)) == intersects
+
+        where:
+        stretch1       | stretch2            | intersects
+        [0, 0, 10, 10] | [0, 5, 5, 5]        | true  // touching in one point
+        [0, 0, 0, 10]  | [0, 0, 10, 0]       | true
+        [0, 0, 10, 0]  | [5, 0, 10, 20]      | true
+        [0, 0, 10, 0]  | [10, 0, 10, 20]     | true
+
+        [0, 0, 0, 10]  | [0.0001, 5, 5, 5]   | false // almost touching
+        [0, 0, 0, 10]  | [1, 0, 1, 10]       | false // parallel
+        [0, 0, 10, 0]  | [0, 0.00001, 1, 10] | false // almost parallel
+        [0, 0, 0, 10]  | [-1, 11, 11, 11]    | false
+        [0, 0, 0, 10]  | [0, 11, 11, 11]     | false
+    }
+
+    def "kolinear stretches are calculated correctly"() {
         expect:
         Stretch.valueOf(stretch1).intersects(Stretch.valueOf(stretch2)) == intersects
 
         where:
         stretch1       | stretch2                   | intersects
-        [0, 0, 0, 10]  | [0, 9, 0, 20]              | true  // on one line
+        [0, 0, 10, 10] | [10.0001, 10.0001, 20, 20] | false
+        [0, 0, 0, 10]  | [0, 11, 0, 20]             | false
+        [0, 0, 0, 10]  | [0, 20, 0, 30]             | false
+        [0, 0, 0, 10]  | [0, 9, 0, 20]              | true
         [0, 0, 0, 20]  | [0, 5, 0, 10]              | true
         [0, 0, 0, 10]  | [0, 5, 0, 20]              | true
-
+        [0, 0, 10, 0]  | [5, 0, 20, 0]              | true  // right
+        [5, 0, 20, 0]  | [0, 0, 10, 0]              | true  // left
         [0, 0, 0, 10]  | [0, 0, 0, 10]              | true  // same stretches
-        [0, 0, 0, 0]   | [0, 0, 0, 0]               | true
 
         [0, 0, 0, 10]  | [0, 10, 0, 20]             | true  // touching in one point
-        [0, 0, 0, 10]  | [0, 10, 0, 10]             | true
         [0, 0, 10, 10] | [10, 10, 20, 20]           | true
-        [0, 0, 10, 10] | [0, 5, 5, 5]               | true
-        [0, 0, 0, 10]  | [0, 0, 10, 0]              | true
-
-        [0, 0, 0, 10]  | [-1, 11, 11, 11]           | false
-
-        [0, 0, 0, 10]  | [0.0001, 5, 5, 5]          | false // almost touching
-        [0, 0, 0, 10]  | [1, 5, 5, 5]               | false
-
-        [0, 0, 0, 10]  | [0, 10, 0, 10]             | true  // one point
-        [0, 0, 0, 10]  | [0, 11, 0, 11]             | false
-        [0, 0, 0, 10]  | [5, 5, 5, 5]               | false
-
-        [0, 0, 0, 0]   | [0.0001, 0, 0.0001, 0]     | false // two points
-        [0, 0, 10, 10] | [11, 11, 20, 20]           | false
-        [0, 0, 0, 0]   | [123, 645, 123, 645]       | false
-        [0, 0, 0, 0]   | [0, 0, 0, 0]               | true
-
-
-//        [0, 0, 0, 10]  | [1, 1, 1, 10]              | false // parallel
-//        [0, 0, 0, 10]  | [0, 0.1, 1, 10]            | false // almost parallel
-
-
-//        [0, 0, 10, 10] | [10.0001, 10.0001, 20, 20] | false // 'kolinear'
-//        [0, 0, 0, 10]  | [0, 11, 0, 20]             | false
-//        [0, 0, 0, 10]  | [0, 20, 0, 30]             | false
+        [0, 0, 0, 10]  | [0, 10, 0, 10]             | true
     }
 
-    def "parallel test"() {
+    def "stretches that are points are calculated correctly"() {
         expect:
-        Stretch.valueOf(stretch).getLeftParallelStetch() == Stretch.valueOf(parallel)
+        Stretch.valueOf(stretch1).intersects(Stretch.valueOf(stretch2)) == intersects
+
+        where:
+        stretch1         | stretch2                  | intersects
+        [0, 0, 0, 10]    | [0, 10, 0, 10]            | true  // one point
+        [0, 0, 0, 10]    | [0, 11, 0, 11]            | false
+        [0, 0, 0, 10]    | [5, 5, 5, 5]              | false
+        [5, 5, 5, 5]     | [0, 0, 0, 10]             | false
+        [10, 10, 10, 10] | [0, 0.099, 4.163, 17.664] | false
+
+        [0, 0, 0, 0]     | [0.0001, 0, 0.0001, 0]    | false // two points
+        [0, 0, 10, 10]   | [11, 11, 20, 20]          | false
+        [0, 0, 0, 0]     | [123, 645, 123, 645]      | false
+        [0, 0, 0, 0]     | [0, 0, 0, 0]              | true  // same points
+        [0, 0, 0, 0]     | [-1, -1, -1, -1]          | false  // left
+        [0, 0, 0, 0]     | [1, 1, 1, 1]              | false  // right
+
+
+    }
+
+    def "parallel left stretch is calculated correctly"() {
+        expect:
+        Stretch.valueOf(stretch).getLeftParallelStretch() == Stretch.valueOf(parallel)
 
         where:
         stretch        | parallel
