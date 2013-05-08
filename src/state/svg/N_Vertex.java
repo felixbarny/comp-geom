5f75ab410da71741071354f9781482d0fdca1ef0
Felix <felix.barnsteiner@gmail.com>
2013-05-08 19:51:53 +0200

    is town in state check

---
+++ src/state/svg/N_Vertex.java
@@ -0,0 +1,34 @@
+package state.svg;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class N_Vertex extends JPanel {
+    final Shape shape;
+
+    public N_Vertex(Shape shape) {
+        this.shape = shape;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        ((Graphics2D) g).draw(shape);
+    }
+
+    public static void paintShape(Shape polygon) {
+        if (polygon == null) return;
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.add(new N_Vertex(polygon));
+        f.setSize(200, 200);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Polygon polygon = new Polygon();
+        polygon.addPoint(0, 0);
+        polygon.addPoint(20, 0);
+        polygon.addPoint(10, 20);
+        paintShape(polygon);
+    }
+}
--- src/state/svg/States.groovy
+++ src/state/svg/States.groovy
@@ -1,34 +1,41 @@
 package state.svg
 
-@Grab(group='org.apache.xmlgraphics', module='batik-parser', version='1.7')
-import intersections.Coordinate
-import org.apache.batik.parser.DefaultPointsHandler
-import org.apache.batik.parser.PointsParser
+@Grab(group = 'org.apache.xmlgraphics', module = 'batik-parser', version = '1.7')
+import org.apache.batik.parser.AWTPathProducer
 import org.apache.batik.parser.ParseException
-import org.apache.batik.parser.PointsHandler
+import org.apache.batik.parser.PathHandler
+import org.apache.batik.parser.PathParser
 
+import java.awt.*
 
-//def svg = new XmlSlurper().parse(new File("./states.svg"))
+println "slurping svg..."
+def svg = new XmlSlurper().parse(new File("./states.svg"))
 
-//println svg.g.path.@id
-def list = extractPoints(new File("./states.svg").text)
-println list
+println "extracting polygons..."
+Map<String, Shape> states = svg.g.path.findAll().collectEntries {
+    if ((it.@d as String).count('z') < 2) {
+        def polygon = extractPoints(it.@d as String)
+//        N_Vertex.paintShape(polygon)
+        [(it.@id): polygon]
+    } else [(it.@id): null]
+}.findAll { it.value != null }
 
-class Polygon {
-//    List<Point> points
+def cities = svg.path.findAll().collectEntries {
+    [(it.@id): [it.@"sodipodi:cx".text() as double, it.@"sodipodi:cy".text() as double]]
 }
 
-public List extractPoints(String s) throws ParseException {
-       final LinkedList points = new LinkedList();
-       PointsParser pp = new PointsParser();
-       PointsHandler ph = new DefaultPointsHandler() {
-           public void point(float x, float y) throws ParseException {
-               points << new Coordinate(x, y)
-           }
-       };
-       pp.setPointsHandler(ph);
-       pp.parse(s);
-       return points;
-   }
+states.each { state ->
+    cities.each { city ->
+        if (state.value.contains(city.value[0], city.value[1])) {
+            println "$city.key ist in $state.key"
+        }
+    }
+}
 
-
+public Shape extractPoints(String s) throws ParseException {
+    PathParser pp = new PathParser();
+    PathHandler awtPathProducer = new AWTPathProducer()
+    pp.setPathHandler(awtPathProducer);
+    pp.parse(s);
+    awtPathProducer.shape
+}
