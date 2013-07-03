dbc3930ee6772d6d582b580163ada1ddf8037940
Najum Ali <n.ali@isys-software.de>
2013-07-03 15:01:22 +0200

    added ccw check

--- src/lec/LEC.m
+++ src/lec/LEC.m
@@ -1,30 +1,28 @@
-% Compute the largest empty circle (LEC)
-
 function LEC(polygon)
-
     disp 'Calculating the largest empty circle inside a convex polygon...';
-
     % detect if first and last coordinates are same and fix matrix if needed
-    validatedPolygon = validate(polygon);
-
-    [rows,columns]= size(validatedPolygon);
+    polygon = validate(polygon);
+    
+    [rows,columns]= size(polygon);
     
     f = [0; 0; -1];
     A = [];
     b = [];
+    disp(ccw(polygon))
+    signccw = sign(ccw(polygon))
     
     % Iterate trough matrix (polygon coordinates)
     for i = 1 : (rows - 1)
         
         % calculate vector from point p to q
-        p = validatedPolygon(i,:)';
-        q = validatedPolygon(i + 1,:)';
+        p = polygon(i,:)';
+        q = polygon(i + 1,:)';
         v = q - p; % "head-minus-tail-rule"
         
         % calculate normal vector (orthogonal to vector v)
-        n = -[-v(2);v(1)];
+        n = signccw * [-v(2);v(1)];
         
-        % normailze vector n (vector length of 1, but some direction)
+        % normailze vector n (vector length of 1, but same direction)
         normalized_n = n/norm(n);
         
         a = [-normalized_n(1) -normalized_n(2) 1];
@@ -38,18 +36,25 @@ function LEC(polygon)
     % use simplex algorithm to solve the linear program
     opts.Algorithm = 'simplex';
     [x,fval,exitflag,output,lambda] = linprog(f,A,b,[],[],[],[],[],opts);
-    
+
     printSolution(abs(x));
     plotSolution(polygon,x);
 end
 
+function ccw = ccw(polygon)
+    p = polygon(1,:);
+    q = polygon(2,:);
+    r = polygon(3,:);
+
+    ccw = ((p(1) * q(2) - p(2) * q(1)) + (q(1) * r(2) - q(2) * r(1)) + (p(2) * r(1) - p(1) * r(2)))
+end
+
 function validPolygon = validate(polygon)
-    [rows,columns]= size(polygon);
+    [rows,columns] = size(polygon);
     if(polygon(1,:) == polygon(rows,:))
-        % cut off last row
-        validPolygon = polygon(1:rows-1,:);
+         validPolygon = polygon;
     else
-        validPolygon = polygon;
+       validPolygon = [polygon;polygon(1,:)];
     end
 end
 
@@ -66,7 +71,6 @@ function plotSolution(polygon,x)
     fill(polygon(:,1),polygon(:,2),'b');
     hold on;
     drawCircle(x(1),x(2),abs(x(3)));
-    radius = abs(x);
 end
 
 % draw filled circle
